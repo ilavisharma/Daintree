31001c2aba56314a80c6a885f4c2db0f78eefffb
Lavi Sharma <lavisharma796@gmail.com>
2022-05-14 19:49:30 +0530

    setup nextjs app with tailwindcss

---
+++ apps/website/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css';
+
+const Index = () => {
+  return (
+    <div className="container">
+      <h1 className="text-center text-2xl">Website</h1>
+    </div>
+  );
+};
+
+export default Index;
